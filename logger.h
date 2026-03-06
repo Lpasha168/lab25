e9648f798af742caa57eef7f0b3bd988eee0bc10
Lpasha168 <lpasa6618@gmail.com>
2026-03-07 00:56:50 +0200

    Update logger.h

--- logger.h
+++ logger.h
@@ -3,8 +3,8 @@
 
 #include <QString>
 #include <QMetaType>
+#include <memory>
 
-// Уровни логирования
 enum class LogLevel {
     Info,
     Warning,
@@ -13,15 +13,30 @@ enum class LogLevel {
 
 Q_DECLARE_METATYPE(LogLevel)
 
-// Базовый интерфейс
 class Logger {
 public:
     virtual ~Logger() = default;
-
     virtual void log(const QString& message, LogLevel level) = 0;
     virtual QString getName() const = 0;
     virtual bool isEnabled() const = 0;
     virtual void setEnabled(bool enabled) = 0;
 };
 
-#endif
+// Композитный логгер для группового логирования
+class CompositeLogger : public Logger {
+public:
+    void addLogger(std::shared_ptr<Logger> logger);
+    void removeLogger(const QString& loggerName);
+    void log(const QString& message, LogLevel level) override;
+    QString getName() const override;
+    bool isEnabled() const override;
+    void setEnabled(bool enabled) override;
+    size_t getLoggerCount() const;
+
+private:
+    std::vector<std::shared_ptr<Logger>> m_loggers;
+    bool m_enabled = true;
+};
+
+#endif 
+
