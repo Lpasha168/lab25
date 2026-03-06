6f1098b21fab8b27a7e888e7cba7725c4bbf151e
Lpasha168 <lpasa6618@gmail.com>
2026-03-07 00:59:24 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
-#include "concrete_loggers.h"
+
+#include "logger_factory.h"
 #include <memory>
 #include <QComboBox>
 #include <QVBoxLayout>
 #include <QPushButton>
 #include <QSettings>
 #include <QInputDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,6 +19,7 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     saveConfig();
+    // Нет необходимости вручную удалять логгеры - shared_ptr сделает это автоматически
 }
 
 void MainWindow::setupUi()
@@ -39,14 +42,17 @@ void MainWindow::setupUi()
     QPushButton* addBtn = new QPushButton("Add Logger", this);
     QPushButton* removeBtn = new QPushButton("Remove Logger", this);
     QPushButton* genBtn = new QPushButton("Generate Log", this);
+    m_createCompositeBtn = new QPushButton("Create Composite Group", this);
 
     connect(addBtn, &QPushButton::clicked, this, &MainWindow::addLogger);
     connect(removeBtn, &QPushButton::clicked, this, &MainWindow::removeLogger);
     connect(genBtn, &QPushButton::clicked, this, &MainWindow::generateMessage);
+    connect(m_createCompositeBtn, &QPushButton::clicked, this, &MainWindow::createCompositeGroup);
 
     layout->addWidget(m_loggerList);
     layout->addWidget(addBtn);
     layout->addWidget(removeBtn);
+    layout->addWidget(m_createCompositeBtn);
     layout->addWidget(m_logDisplay);
     layout->addWidget(m_messageInput);
     layout->addWidget(m_levelCombo);
@@ -63,14 +69,20 @@ void MainWindow::addLogger()
                                          "Select type:", types, 0, false, &ok);
     if (!ok) return;
 
+    std::shared_ptr<Logger> logger;
+
     if (type == "File")
-        m_loggers.push_back(std::make_unique<FileLogger>("application.log"));
+        logger = LoggerFactory::createLogger(LoggerType::File, "application.log");
     else if (type == "Console")
-        m_loggers.push_back(std::make_unique<ConsoleLogger>());
+        logger = LoggerFactory::createLogger(LoggerType::Console);
     else if (type == "GUI")
-        m_loggers.push_back(std::make_unique<GuiLogger>(m_logDisplay));
+        logger = LoggerFactory::createLogger(LoggerType::Gui, "", m_logDisplay);
     else if (type == "Network")
-        m_loggers.push_back(std::make_unique<NetworkLogger>());
+        logger = LoggerFactory::createLogger(LoggerType::Network);
+
+    if (logger) {
+        m_loggers.push_back(logger);
+    }
 
     refreshLoggerList();
 }
@@ -80,15 +92,39 @@ void MainWindow::removeLogger()
     int row = m_loggerList->currentRow();
     if (row < 0) return;
 
+    // Просто удаляем из вектора - память освободится автоматически
     m_loggers.erase(m_loggers.begin() + row);
     refreshLoggerList();
+}
+
+void MainWindow::createCompositeGroup()
+{
+    if (m_loggers.empty()) {
+        QMessageBox::information(this, "Info", "No loggers to group");
+        return;
+    }
+
+    // Создаем композитный логгер из всех существующих логгеров
+    auto composite = LoggerFactory::createCompositeLogger(m_loggers);
+
+    // Добавляем композитный логгер как новый логгер
+    m_loggers.push_back(composite);
+    m_compositeGroups.push_back(composite);
+
+    refreshLoggerList();
+
+    QMessageBox::information(this, "Success",
+                             "Composite group created with " + QString::number(composite->getLoggerCount()) + " loggers");
 }
 
 void MainWindow::refreshLoggerList()
 {
     m_loggerList->clear();
-    for (const auto& logger : m_loggers)
-        m_loggerList->addItem(logger->getName());
+    for (const auto& logger : m_loggers) {
+        if (logger) {
+            m_loggerList->addItem(logger->getName());
+        }
+    }
 }
 
 void MainWindow::generateMessage()
@@ -101,8 +137,12 @@ void MainWindow::generateMessage()
     else if (m_levelCombo->currentIndex() == 2)
         level = LogLevel::Error;
 
-    for (const auto& logger : m_loggers)
-        logger->log(text, level);
+    // Логируем сообщение через все логгеры
+    for (const auto& logger : m_loggers) {
+        if (logger && logger->isEnabled()) {
+            logger->log(text, level);
+        }
+    }
 }
 
 void MainWindow::saveConfig()
@@ -110,8 +150,8 @@ void MainWindow::saveConfig()
     QSettings settings("MyCompany", "LogSystem");
 
     settings.beginWriteArray("loggers");
-    for (int i = 0; i < m_loggers.size(); ++i) {
-        settings.setArrayIndex(i);
+    for (size_t i = 0; i < m_loggers.size(); ++i) {
+        settings.setArrayIndex(static_cast<int>(i));
         settings.setValue("type", m_loggers[i]->getName());
     }
     settings.endArray();
@@ -126,14 +166,22 @@ void MainWindow::loadConfig()
         settings.setArrayIndex(i);
         QString type = settings.value("type").toString();
 
+        std::shared_ptr<Logger> logger;
+
         if (type == "File Logger")
-            m_loggers.push_back(std::make_unique<FileLogger>("application.log"));
+            logger = LoggerFactory::createLogger(LoggerType::File, "application.log");
         else if (type == "Console Logger")
-            m_loggers.push_back(std::make_unique<ConsoleLogger>());
+            logger = LoggerFactory::createLogger(LoggerType::Console);
         else if (type == "GUI Logger")
-            m_loggers.push_back(std::make_unique<GuiLogger>(m_logDisplay));
+            logger = LoggerFactory::createLogger(LoggerType::Gui, "", m_logDisplay);
         else if (type == "Network Logger")
-            m_loggers.push_back(std::make_unique<NetworkLogger>());
+            logger = LoggerFactory::createLogger(LoggerType::Network);
+        else if (type.startsWith("Composite Logger"))
+            logger = LoggerFactory::createLogger(LoggerType::Composite);
+
+        if (logger) {
+            m_loggers.push_back(logger);
+        }
     }
     settings.endArray();
 
