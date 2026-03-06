3d4680fa39e427118011271b4d36a122695d4cad
Lpasha168 <lpasa6618@gmail.com>
2026-03-07 00:57:39 +0200

    Update logger_factory.h

--- logger_factory.h
+++ logger_factory.h
@@ -1,6 +1,5 @@
 #ifndef LOGGER_FACTORY_H
 #define LOGGER_FACTORY_H
-
 #include "logger.h"
 #include <memory>
 #include <QTextEdit>
@@ -10,15 +9,17 @@ enum class LoggerType {
     File,
     Console,
     Gui,
-    Network
+    Network,
+    Composite
 };
 
 class LoggerFactory {
 public:
-    // Статический метод-фабрика. Возвращает умный указатель (unique_ptr)
-    static std::unique_ptr<Logger> createLogger(LoggerType type,
-                                                const QString& param = "",
-                                                QTextEdit* textEdit = nullptr);
+    // Статический метод-фабрика. Возвращает умный указатель (shared_ptr)
+    static std::shared_ptr<Logger> createLogger(LoggerType type,const QString& param = "",QTextEdit* textEdit = nullptr);
+    // Создает композитный логгер с предустановленными логгерами
+    static std::shared_ptr<CompositeLogger> createCompositeLogger(
+        const std::vector<std::shared_ptr<Logger>>& loggers = {});
 };
 
 #endif // LOGGER_FACTORY_H
